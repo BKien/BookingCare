1a39dab38d7c742c982782d6740495b2c30e18e1
Kein24020187 <thekien2006@gmail.com>
2026-01-16 03:05:24 +0700

    Feat: add header section and search section for Home Page

--- bookingcare-clone/frontend/src/components/Header/Header.jsx
+++ bookingcare-clone/frontend/src/components/Header/Header.jsx
@@ -1,5 +1,56 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import './Header.scss'
+import menuLogo from "../../assets/images/menu_icon.svg"
+import bookingCareLogo from "../../assets/images/bookingcarelogo.svg"
+import scheduleLogo from "../../assets/images/schedule_logo.svg"
 const Header = () => {
-  return <header>Header</header>;
+  return(
+    <div className="header">
+      <div className="header-container">
+        
+        <div className="left-side">
+          <img src={menuLogo} className="menu-logo"></img>
+          <a href="/"><img src={bookingCareLogo} className="bookingcare-logo"></img></a>
+        </div>
+
+        <div className="center">
+          <NavLink
+            to="/all"
+            className={({isActive})=>`nav-item ${isActive?'active':''}`}
+          >
+            Tất Cả
+          </NavLink>
+
+          <NavLink
+            to="/home-care"
+            className={({isActive})=>`nav-item ${isActive?'active':''}`}
+          >
+            Tại Nhà
+          </NavLink>
+
+          <NavLink
+            to="/hospital"
+            className={({isActive})=>`nav-item ${isActive?'active':''}`}
+          >
+            Tại Viện
+          </NavLink>
+
+          <NavLink
+            to="/wellness"
+            className={({isActive})=>`nav-item ${isActive?'active':''}`}
+          >
+            Sống Khỏe
+          </NavLink>
+        </div>
+
+        <div className="right-side">
+          <a href="/schedule"><img src={scheduleLogo} className="shedule-logo"></img></a>
+          <a href="/login"> <button className="login-button">Đăng nhập</button></a>  
+        </div>
+    </div>
+    </div>
+  )
 };
 
 export default Header;
--- bookingcare-clone/frontend/src/pages/Home/components/SearchSection.jsx
+++ bookingcare-clone/frontend/src/pages/Home/components/SearchSection.jsx
@@ -1,6 +1,18 @@
-
+import "./SearchSection.scss"
+import searchButton from "../../../assets/images/search_button.png"
 const SearchSection = ()=>{
 
+    return(
+        <div className="search-section">
+            <div className="lable">Nền tảng đặt lịch khám bệnh, chăm sóc răng miệng và làm đẹp</div>
+            <div className="search-container">
+                <input className="search-input" placeholder="Tìm tên bác sĩ">
+                </input>
+                <img className="search-icon" src={searchButton}></img>
+            </div>
+        </div>
+
+    )
 }
 
 export default SearchSection
